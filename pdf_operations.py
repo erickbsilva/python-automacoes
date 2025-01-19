c23eed7385c774cea2dd56bf8f9cbee1c8e4b5f2
Erick <erickbarretodasilva@gmail.com>
2025-01-19 19:23:40 -0300

    rotacionando e unindo pdfs

--- pdf_operations.py
+++ pdf_operations.py
@@ -1,5 +1,5 @@
 import PyPDF2 as pdf
-from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2 import PdfReader, PdfWriter, PdfMerger
 import os
 
 
@@ -35,11 +35,62 @@ def split_pdf(pdf_path):
             print(f"PDF criado com o nome: {output_filename}")
 
 
-split_pdf("files/sample2.pdf")
+def get_pdf_upto(pdf_path, start_page: int = 0, stop_page: int = 0):
+    with open(pdf_path, "rb") as f:
+        reader = PdfReader(f)
+        writer = PdfWriter()
+        for page_num in range(start_page, stop_page):
+            selected_page = reader.pages[page_num]
+            writer.add_page(selected_page)
+            filename = os.path.split(pdf_path)[1]
+            output_filename = (
+                f"files/{filename}_from_{start_page+1}_to_{stop_page+1}.pdf"
+            )
+        with open(output_filename, "wb") as out:
+            writer.write(out)
 
 
-print(get_pdf_metadata("files/sample.pdf"))
-print(get_pdf_metadata("files/sample.pdf").title)
-print(get_pdf_metadata("files/sample.pdf").author)
+def fetch_all_pdf_files(parent_folder: str):
+    target_files = []
+    for path, subdirs, files in os.walk(parent_folder):
+        for name in files:
+            if name.endswith(".pdf"):
+                target_files.append(os.path.join(path, name))
+    return target_files
 
-print(extract_text_from_pdf("files/sample.pdf"))
+
+def merge_pdf(list_pdfs, output_filename="files/final_pdf.pdf"):
+    merger = PdfMerger()
+    with open(output_filename, "wb") as f:
+        for file in list_pdfs:
+            merger.append(file)
+        merger.write(f)
+
+
+def rotate_pdf(pdf_path, page_num: int, rotation: int = 90):
+    with open(pdf_path, "rb") as f:
+        reader = PdfReader(f)
+        writer = PdfWriter()
+        writer.add_page(reader.pages[page_num])
+        writer.pages[page_num].rotate(rotation)
+        filename = os.path.split(pdf_path)[1]
+        output_filename = f"files/{filename}_{rotation}_rotated_page.pdf"
+        with open(output_filename, "wb") as out:
+            writer.write(out)
+
+
+# split_pdf("files/sample2.pdf")
+# get_pdf_upto("files/sample2.pdf", 1, 2)
+
+# print(fetch_all_pdf_files("files/"))
+
+# pdf_list = fetch_all_pdf_files("files/")
+# merge_pdf(pdf_list)
+
+rotate_pdf("files/sample.pdf", 0)
+
+# print(get_pdf_metadata("files/sample.pdf"))
+# print(get_pdf_metadata("files/sample.pdf").title)
+# print(get_pdf_metadata("files/sample.pdf").author)
+
+# print(extract_text_from_pdf("files/sample.pdf"))
