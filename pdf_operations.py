a481fb2510bbb67cdc5aad95590db948e93f1904
Erick <erickbarretodasilva@gmail.com>
2025-01-19 18:59:07 -0300

    separando pdfs por páginas

--- pdf_operations.py
+++ pdf_operations.py
@@ -1,5 +1,6 @@
 import PyPDF2 as pdf
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
+import os
 
 
 def get_pdf_metadata(pdf_path):
@@ -18,6 +19,23 @@ def extract_text_from_pdf(pdf_path):
             text = selected_page.extract_text()
             results.append(text)
         return " ".join(results)
+
+
+def split_pdf(pdf_path):
+    with open(pdf_path, "rb") as f:
+        reader = PdfReader(f)
+        for page_num in range(0, len(reader.pages)):
+            selected_page = reader.pages[page_num]
+            writer = PdfWriter()
+            writer.add_page(selected_page)
+            filename = os.path.split(pdf_path)[1]
+            output_filename = f"files/{filename}_{page_num+1}.pdf"
+            with open(output_filename, "wb") as out:
+                writer.write(out)
+            print(f"PDF criado com o nome: {output_filename}")
+
+
+split_pdf("files/sample2.pdf")
 
 
 print(get_pdf_metadata("files/sample.pdf"))
