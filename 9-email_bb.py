91be85cbabebd04afc0287a39b4a09f7f4c598fe
Erick <erickbarretodasilva@gmail.com>
2025-01-18 23:29:49 -0300

    consultando uma ação e disparando email

---
+++ 9-email_bb.py
@@ -0,0 +1,36 @@
+from email.message import EmailMessage
+import smtplib
+import ssl
+import mimetypes
+
+password = str(open("senha", "r").read())
+from_email = "6ff1fcdbbd7ad8"
+to_email = "erickbarretodasilva@gmail.com"
+subject = "Curso Python"
+body = open("files/corpo_bb.txt", "r", encoding="utf-8").read()
+
+message = EmailMessage()
+message["From"] = from_email
+message["To"] = to_email
+message["Subject"] = subject
+
+message.set_content(body, subtype="html")
+
+anexo = "bb_preco.png"
+print(mimetypes.guess_type(anexo)[0])
+mime_type, mime_subtype = mimetypes.guess_type(anexo)[0].split("/")
+with open(anexo, "rb") as a:
+    message.add_attachment(
+        a.read(), maintype=mime_type, subtype=mime_subtype, filename=anexo
+    )
+
+try:
+    with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as smtp:
+        smtp.starttls()
+        smtp.login(from_email, password)
+        smtp.sendmail(
+            "Private Person <from@example.com>", to_email, message.as_string()
+        )
+    print("Email enviado com sucesso!")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
